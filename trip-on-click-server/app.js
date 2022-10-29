17ad666cabf109fc2ef1b3e6403025a74358cb8d
levo2001 <levonahaim@gmail.com>
2022-10-30 01:06:27 +0200

    create server

--- trip-on-click-server/app.js
+++ trip-on-click-server/app.js
@@ -16,15 +16,15 @@ app.use(cors());
 app.use(express.json());//req.body.message
 app.use(express.urlencoded({extended: false}));
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    if (req.method === "OPTIONS") {
-        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-        return res.status(200).json({});
-    }
-    next();
-});
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+//     if (req.method === "OPTIONS") {
+//         res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+//         return res.status(200).json({});
+//     }
+//     next();
+// });
 
 
 
